15f1d581e38b01ee50bde3371b0927718e8ab649
Lucas da Silva Santos <lucas.parkl@outlook.com>
2023-02-08 19:55:01 -0300

    implementação autenticação Client Credentials projeto Java SpringBoot: - realizando autenticação de request utilizando Spring Security + Keycloak

---
+++ aplicação-java/spring-com-keycloak-exemplo/src/main/java/com/lucassilvs/springcomkeycloakexemplo/SpringComKeycloakExemploApplication.java
@@ -0,0 +1,13 @@
+package com.lucassilvs.springcomkeycloakexemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringComKeycloakExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringComKeycloakExemploApplication.class, args);
+	}
+
+}
---
+++ aplicação-java/spring-com-keycloak-exemplo/src/main/java/com/lucassilvs/springcomkeycloakexemplo/controller/PortalController.java
@@ -0,0 +1,19 @@
+package com.lucassilvs.springcomkeycloakexemplo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping("/security")
+@RestController
+public class PortalController {
+
+    @GetMapping
+    public ResponseEntity<String> security(){
+        return ResponseEntity.ok("Autorizado");
+
+    }
+
+}
