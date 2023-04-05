ac9868825839a1eaf94381064ff37efea5c0cab6
Lucas Santos <lucas.parkl@outlook.com>
2023-04-05 19:21:01 -0300

    wip: adicionando cache da key para validação do JWT Spring Security:
    - adicionando Decoder com o cache da chave no spring security
    - adicionando dependencia Google Guava para implementação do CacheBuilder

--- aplicação-java/spring-com-keycloak-exemplo/src/main/java/com/lucassilvs/springcomkeycloakexemplo/configuration/security/SecurityConfiguration.java
+++ aplicação-java/spring-com-keycloak-exemplo/src/main/java/com/lucassilvs/springcomkeycloakexemplo/configuration/security/SecurityConfiguration.java
@@ -1,13 +1,22 @@
 package com.lucassilvs.springcomkeycloakexemplo.configuration.security;
 
+import com.google.common.cache.CacheBuilder;
 import com.lucassilvs.springcomkeycloakexemplo.configuration.KeycloakJwtAuthenticationConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -16,6 +25,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     private final KeycloakJwtAuthenticationConverter keycloakJwtAuthenticationConverter;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
 
 
     public SecurityConfiguration(KeycloakJwtAuthenticationConverter keycloakJwtAuthenticationConverter) {
@@ -27,10 +39,22 @@ public class SecurityConfiguration {
         http
                 .authorizeHttpRequests(authz -> authz.requestMatchers("/security/**").authenticated())
                 .oauth2ResourceServer()
-                .jwt().jwtAuthenticationConverter(keycloakJwtAuthenticationConverter);
-
+                .jwt()
+                .decoder(jwksCacheDecoder())
+                .jwtAuthenticationConverter(keycloakJwtAuthenticationConverter);
 
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwksCacheDecoder(){
+        var jwkSetCache = new ConcurrentMapCache("jwkSetCache", CacheBuilder.newBuilder()
+                .expireAfterWrite(Duration.ofMinutes(30))
+                .build().asMap(), false);
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri)
+                .restOperations(new RestTemplateBuilder().build())
+                .cache(jwkSetCache)
+                .build();
 
     }
 }
